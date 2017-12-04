aa593b669c8f959ed7d883563de19580ac11a78c
gbreux <contact@guillaumebreux.com>
2017-12-04 19:35:30 +0100

    Transform picture to svg placeholder

---
+++ sqip.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const _ = require('underscore');
+const fs = require('fs');
+const path = require('path');
+const sqip = require('sqip');
+
+const outputDir = './svg/';
+const regexImgs = /\.(jpe?g|png|gif|svg)/i;
+
+fs.readdir('./i/', function(err, files) {
+  files = files.filter(f => f !== '.DS_Store');
+  fs.readdir('./svg/', function(err, svgs) {
+    files = files.filter((f) => {
+      const fileName = f.replace(regexImgs, '');
+      return !~svgs.indexOf(fileName + '.svg')
+    });
+    files.filter(f => f !== '.DS_Store').map(f => {
+      const result =  sqip({
+        filename: './i/'+f,
+        numberOfPrimitives: 8
+      });
+      if (fs.existsSync(outputDir) || fs.mkdirSync(outputDir)) {
+        fs.writeFileSync(path.join(outputDir, f.replace(regexImgs, '.svg')), result.final_svg);
+      }
+    })
+  })
+});
