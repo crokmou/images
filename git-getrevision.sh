288fbd226ad44866ca307a774ce3b632b94a8789
gbreux <contact@guillaumebreux.com>
2017-12-04 20:52:19 +0100

    Auto commit

--- git-getrevision.sh
+++ git-getrevision.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git add -A && git diff-index --quiet HEAD || git commit -m 'Auto commit' && git push
 projectversion=`git describe --tags --long`
+git tag -a $projectversion -m 'Auto tag';
+git push --tags
 
 echo "$projectversion"
