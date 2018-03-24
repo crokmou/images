3edd5a82976e9f4853a46eb1ad47310eff412dbd
gbreux <contact@guillaumebreux.com>
2018-03-24 13:57:33 +0100

    Add missing webp and fix resize script

--- _scripts/resize.js
+++ _scripts/resize.js
@@ -6,12 +6,16 @@ const files = getFiles('i', true);
 const p14 = getFiles('i/p14');
 const p750 = getFiles('i/p750');
 const p750x450 = getFiles('i/p750x450');
+const webp = getFiles('webp', true).map(i => i.name.replace(/\.webp$/i, ''));
 const wP14 = getFiles('webp/p14').map(i => i.name.replace(/\.webp$/i, ''));
 const wP750 = getFiles('webp/p750').map(i => i.name.replace(/\.webp$/i, ''));
 const wP750x450 = getFiles('webp/p750x450').map(i => i.name.replace(/\.webp$/i, ''));
 
 files.map((file) => {
   if(isNotFolder(file.name)) {
+    if(/.jpe?g$/.test(file.name) && !~webp.indexOf(file.name.replace(/(\.jpe?g)$/i, ''))) {
+      sharp(file.path).toFile(file.path.replace(/^i\//, 'webp/').replace(/(\.jpe?g)$/i, '.webp'));
+    }
     if(!~p14.map(i => i.name).indexOf(file.name) || !~wP14.indexOf(file.name.replace(/\.(jpe?g|gif|png|bmp|webp)/i, ''))) {
       const folder = 'i/p14/';
       if (fs.existsSync(folder) || fs.mkdirSync(folder)) {
